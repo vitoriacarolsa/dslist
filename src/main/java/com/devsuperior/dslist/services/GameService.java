39f505066c46ae49f43df9d59a9fb1c60991d683
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-04-12 01:06:17 -0300

    games by lists

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,8 @@ public class GameService {
     @Transactional (readOnly = true)
     public GameDTO findById(Long id){
         Game result= gameRepository.findById(id).get();
-        GameDTO dto = new GameDTO(result);
-        return dto;
+        return new GameDTO(result);
+
     }
 
     @Transactional (readOnly = true)
@@ -27,5 +28,11 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
        return result.stream().map(x-> new GameMinDTO(x)).toList();
 
+    }
+
+    @Transactional (readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+       List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x->new GameMinDTO(x)).toList();
     }
 }
